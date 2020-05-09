472072f74056216452f84415c8d8e22bb9445c81
qodjf <qianlifengxian@gmail.com>
2020-05-10 02:53:34 +0800

    update segment tree

--- Data_Structures/segment_tree.cpp
+++ Data_Structures/segment_tree.cpp
@@ -1,98 +1,94 @@
-template <typename valueType, typename tagType>
+#include <bits/stdc++.h>
+using namespace std;
+
+template <typename Value, typename Tag>
 class SegmentTreeNode {
  public:
-  valueType val;
-  tagType tag;
+  // [l, r)
+  int l, r;
+  Value val;
+  Tag tag;
 };
 
-template <typename valueType, typename tagType,
-          valueType (*merge)(valueType, valueType),
-          void (*update)(SegmentTreeNode<valueType, tagType>&, tagType)>
+template <typename Value, typename Tag, Value (*merge)(Value, Value),
+          void (*update)(SegmentTreeNode<Value, Tag>&, Tag)>
 class SegmentTree {
  private:
-  std::vector<SegmentTreeNode<valueType, tagType>> nodes;
+  std::vector<SegmentTreeNode<Value, Tag>> nodes;
+  // segment: [leftRange, rightRange)
   int leftRange, rightRange;
-  valueType valueZero;
-  tagType tagZero;
+  Value valueZero;
+  Tag tagZero;
+
+  inline int left(int cur) { return 2 * cur + 1; }
+  inline int right(int cur) { return 2 * cur + 2; }
 
   void pushup(int cur) {
-    nodes[cur].val = merge(nodes[cur << 1].val, nodes[cur << 1 | 1].val);
+    nodes[cur].val = merge(nodes[left(cur)].val, nodes[right(cur)].val);
   }
 
   void pushdown(int cur) {
-    update(nodes[cur << 1], nodes[cur].tag);
-    update(nodes[cur << 1 | 1], nodes[cur].tag);
+    update(nodes[left(cur)], nodes[cur].tag);
+    update(nodes[right(cur)], nodes[cur].tag);
     nodes[cur].tag = tagZero;
   }
 
-  void build(int cur, int l, int r, const std::vector<valueType>& initValue) {
+  void build(int cur, int l, int r, const std::vector<Value>& a) {
+    nodes[cur].l = l;
+    nodes[cur].r = r;
     nodes[cur].tag = tagZero;
     if (l == r - 1)
-      nodes[cur].val = initValue[l - leftRange];
+      nodes[cur].val = a[l - leftRange];
     else {
-      build(cur << 1, l, (l + r) >> 1, initValue);
-      build(cur << 1 | 1, (l + r) >> 1, r, initValue);
+      build(left(cur), l, (l + r) >> 1, a);
+      build(right(cur), (l + r) >> 1, r, a);
       pushup(cur);
     }
   }
 
-  void init(const std::vector<valueType>& _initValue,
-            const valueType& _valueZero, const tagType& _tagZero) {
-    valueZero = _valueZero;
-    tagZero = _tagZero;
-    nodes.resize((rightRange - leftRange) << 2);
-    build(1, leftRange, rightRange, _initValue);
-  }
-
-  void modify(int cur, int l, int r, int L, int R, const tagType& tag) {
+  void modify(int cur, int l, int r, int L, int R, const Tag& tag) {
     if (l >= R || r <= L) return;
     if (L <= l && r <= R)
       update(nodes[cur], tag);
     else {
       pushdown(cur);
-      modify(cur << 1, l, (l + r) >> 1, L, R, tag);
-      modify(cur << 1 | 1, (l + r) >> 1, r, L, R, tag);
+      modify(left(cur), l, (l + r) >> 1, L, R, tag);
+      modify(right(cur), (l + r) >> 1, r, L, R, tag);
       pushup(cur);
     }
   }
 
-  valueType query(int cur, int l, int r, int L, int R) {
+  Value query(int cur, int l, int r, int L, int R) {
     if (l >= R || r <= L) return valueZero;
     if (L <= l && r <= R) return nodes[cur].val;
     pushdown(cur);
-    return merge(query(cur << 1, l, (l + r) >> 1, L, R),
-                 query(cur << 1 | 1, (l + r) >> 1, r, L, R));
+    return merge(query(left(cur), l, (l + r) >> 1, L, R),
+                 query(right(cur), (l + r) >> 1, r, L, R));
   }
 
  public:
-  SegmentTree() {}
-
-  SegmentTree(int _leftRange, int _rightRange,
-              const std::vector<valueType>& _initValue,
-              const valueType& _valueZero, const tagType& _tagZero) {
+  /**
+   * use a[0, rightRange - leftRange) to init semgent: [leftRange, rightRange)
+   */
+  SegmentTree(const Value& _valueZero, const Tag& _tagZero, int _leftRange,
+              int _rightRange, const std::vector<Value>& a) {
     leftRange = _leftRange;
     rightRange = _rightRange;
-    init(_initValue, _valueZero, _tagZero);
+    valueZero = _valueZero;
+    tagZero = _tagZero;
+    nodes.resize((rightRange - leftRange) << 2);
+    build(0, leftRange, rightRange, a);
   }
 
-  SegmentTree(int size, const std::vector<valueType>& _initValue,
-              const valueType& _valueZero, const tagType& _tagZero) {
-    leftRange = 1;
-    rightRange = size + 1;
-    init(_initValue, _valueZero, _tagZero);
+  void modify(int l, int r, const Tag& tag) {
+    modify(0, leftRange, rightRange, l, r, tag);
   }
 
-  void modify(int l, int r, const tagType& tag) {
-    modify(1, leftRange, rightRange, l, r, tag);
-  }
+  void modify(int p, const Tag& tag) { modify(p, p + 1, tag); }
 
-  void modify(int p, const tagType& tag) { modify(p, p + 1, tag); }
+  Value query(int l, int r) { return query(0, leftRange, rightRange, l, r); }
 
-  valueType query(int l, int r) {
-    return query(1, leftRange, rightRange, l, r);
-  }
-
-  valueType query(int p) { return query(p, p + 1); }
+  Value query(int p) { return query(p, p + 1); }
 };
 
 // all code are copied from
@@ -109,11 +105,15 @@ struct Tag {
   }
 };
 
-int merge(int x, int y) { return x + y; }
+using Value = int;
+Value merge(Value x, Value y) { return x + y; }
 
-void update(SegmentTreeNode<int, Tag>& node, Tag tag) {
-  node.val = node.val * tag.mul + tag.add * (node.right - node.left);
+void update(SegmentTreeNode<Value, Tag>& node, Tag tag) {
+  node.val = node.val * tag.mul + tag.add * (node.r - node.l);
   node.tag += tag;
 }
 
-SegmentTree<int, Tag, merge, update> t;
+int main() {
+  vector<Value> a(3, 0);
+  SegmentTree<Value, Tag, merge, update> t(0, Tag(), 0, 3, a);
+}
