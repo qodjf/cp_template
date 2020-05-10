fac299860a12f99aca7732eb8726baaab2e5e42f
qodjf <qianlifengxian@gmail.com>
2020-05-10 13:50:50 +0800

    update

--- Data_Structures/dsu.cpp
+++ Data_Structures/dsu.cpp
@@ -1,3 +1,6 @@
+#include <bits/stdc++.h>
+using namespace std;
+
 struct DSU {
   vector<int> fa;
   DSU(int n) {
@@ -7,4 +10,11 @@ struct DSU {
   int find(int x) { return x == fa[x] ? x : fa[x] = find(fa[x]); }
   void merge(int x, int y) { fa[find(y)] = find(x); }
   bool test(int x, int y) { return find(x) == find(y); }
+  int countDS() {
+    int cnt = 0;
+    for (int i = 0; i < fa.size(); i++) {
+      if (find(i) == i) cnt++;
+    }
+    return cnt;
+  }
 };
--- Graph/min_spanning_tree.cpp
+++ Graph/min_spanning_tree.cpp
@@ -11,6 +11,13 @@ struct DSU {
   int find(int x) { return x == fa[x] ? x : fa[x] = find(fa[x]); }
   void merge(int x, int y) { fa[find(y)] = find(x); }
   bool test(int x, int y) { return find(x) == find(y); }
+  int countDS() {
+    int cnt = 0;
+    for (int i = 0; i < fa.size(); i++) {
+      if (find(i) == i) cnt++;
+    }
+    return cnt;
+  }
 };
 
 struct edge {
